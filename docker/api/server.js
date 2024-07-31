45bd5ab7b91bb052e79432610ee7804da6f85290
DavidTheArduinoFighter <65223452+DavidTheArduinoFighter@users.noreply.github.com>
2024-07-31 18:40:47 +0200

    Dynamic reading from json files

--- docker/api/server.js
+++ docker/api/server.js
@@ -1,21 +1,37 @@
-const  express = require('express');
+const express = require('express');
+const fs = require('fs');
+
 const app = express();
 
 const port = 4000;
 
-const fs = require('fs');
-let twelveCredentials;
-fs.readFile('./credentials/twelveCredentials.json', 'utf8', function (err, data) {
-  if (err) throw err;
-  twelveCredentials = JSON.parse(data);
-});
+
+const jsonFilePathTwelve = './json/credentials/twelveCredentials.json';
+const jsonFilePathSymbols = './json/symbols/symbols.json';
 
 app.listen(port, () => {
     console.log(`Server is running, listening at http://localhost:${port}`);
 });
 
+
 app.get('/twelveData', function (req, res) {
-    res.json(twelveCredentials)
+    fs.readFile(jsonFilePathTwelve, 'utf8', function (err, data) {
+        if (err) throw err;
+        const twelveCredentials = JSON.parse(data);
+        console.log(`Twelve json retrieved.`);
+        res.json(twelveCredentials)
+        console.log(`Twelve data posted.`);
+    });
+});
+
+
+app.get('/symbols', function (req, res) {
+    fs.readFile(jsonFilePathSymbols, 'utf8', function (err, data) {
+        if (err) throw err;
+        console.log(`Symbols json retrieved.`);
+        res.json(JSON.parse(data));
+        console.log(`Symbols data posted.`);
+    });
 });
 
 app.use(express.urlencoded({
---
+++ docker/python/symbols.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def get_symbols():
+    url = "http://localhost:4000/symbols"
+    return requests.get(url).json()
+
+def stocks():
+    url = "http://localhost:4000/symbols"
+    return requests.get(url).json()['logInCredentials']['twelveApiKey']
