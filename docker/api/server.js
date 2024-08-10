c339e695d60f3ce61711d2bff6cbffcac5a74604
DavidTheArduinoFighter <65223452+DavidTheArduinoFighter@users.noreply.github.com>
2024-08-10 18:09:42 +0200

    API Server adjust to post new symbols

--- docker/api/server.js
+++ docker/api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -12,6 +13,9 @@ const jsonFilePathSymbols = './json/symbols/symbols.json';
 app.listen(port, () => {
     console.log(`Server is running, listening at http://localhost:${port}`);
 });
+
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 
 app.get('/twelveData', function (req, res) {
@@ -34,6 +38,32 @@ app.get('/symbols', function (req, res) {
     });
 });
 
+app.post('/symbols', async function (req, res) {
+    const result = req.body;
+    const fileData = await readSymbolsFile();
+    const updatedArray = fileData[(result.symbolTaype)]
+    if(!updatedArray.includes(result.symbol)) {
+        updatedArray.push(result.symbol)
+        fs.writeFileSync(jsonFilePathSymbols, JSON.stringify(fileData));
+        res.status(200).send('Data written in system!');
+    }
+    else {
+        res.status(200).send('Data already in system!');
+    }
+})
+
 app.use(express.urlencoded({
     extended: true
 }));
+
+function readSymbolsFile() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(jsonFilePathSymbols, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(data));
+            }
+        });
+    });
+}
