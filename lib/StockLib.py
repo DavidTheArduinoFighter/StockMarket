23bb03972ef31ebc1c2443a27ef456e8f6909c8d
DavidTheArduinoFighter <65223452+DavidTheArduinoFighter@users.noreply.github.com>
2024-09-13 17:04:25 +0200

    Benchmark method added

--- lib/StockLib.py
+++ lib/StockLib.py
@@ -3,59 +3,69 @@ from docker.python.symbols import get_symbols
 
 
 # Dic to store stock symbols and their tables
-symbol_table_mapping = None
+symbol_table_mapping = dict()
+benchmark_table_name = None
 
 
 def fetch_symbol_data():
     """Fetch JSON data of stock symbols and build the dictionary."""
     global symbol_table_mapping
-    if symbol_table_mapping is None:
+    global benchmark_table_name
+
+    if not symbol_table_mapping:
         print("Fetching symbol data for the first time...")
 
         symbol_data = get_symbols()
 
         symbol_table_mapping = {}
         for category in symbol_data:
-            for item in symbol_data[category]:
-                symbol = item['symbol']
-                table_name = item['table']
-                symbol_table_mapping[symbol] = table_name
+            if category == 'benchmark':
+                benchmark_table_name = symbol_data[category][0]['table']
+            else:
+                extract_symbol_data(symbol_data[category], symbol_table_mapping)
+
     else:
         print("Symbol data already loaded.")
 
 
+def extract_symbol_data(symbol_data, dict_name):
+    for item in symbol_data:
+        symbol = item['symbol']
+        table_name = item['table']
+        dict_name[symbol] = table_name
+
+
 def get_table_name(symbol):
     """Fetch the table name for the specified stock symbol."""
-    if symbol_table_mapping is None:
+    if not symbol_table_mapping:
         fetch_symbol_data()  # Load symbol data only once
     return symbol_table_mapping.get(symbol, None)
 
 
-def query_db_for_symbol(symbol):
-    """Query the database for the specified stock symbol using the table name."""
+def data_for_symbol(symbol):
     table_name = get_table_name(symbol)
     if table_name is None:
         raise ValueError(f"No table found for symbol: {symbol}")
 
-    # Connect to the database using the credentials module
+    return get_db_data(table_name)
+
+
+def get_db_data(table_name):
     conn = connect_to_db()
     cur = conn.cursor()
-
     cur.execute(
         f'SELECT price_date, open_price, close_price, high_price, low_price, volume FROM {table_name} n ORDER BY price_date ASC'
     )
+    stock_data = cur.fetchall()
 
     disconnect_from_db(conn)
-
-    stock_data = cur.fetchall()
 
     return stock_data
 
 
 def get_stock_data(symbol):
-    return query_db_for_symbol(symbol)
+    return data_for_symbol(symbol)
 
 
 def get_benchmark_data():
-    # TODO
-    pass
+    return get_db_data(benchmark_table_name)
