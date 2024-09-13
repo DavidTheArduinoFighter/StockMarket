f7b65129d81ecdbc9659c0b122cd22c81b25fb45
DavidTheArduinoFighter <65223452+DavidTheArduinoFighter@users.noreply.github.com>
2024-09-13 22:33:28 +0200

    Setup file for lib added

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Stocklib',
+    version='1.1.0',
+    description='A library to manage stock data and fetch from DB',
+    author='David Ogorevc',
+    url='https://github.com/DavidTheArduinoFighter/StockMarket',
+    packages=find_packages(include=['lib', 'docker.python']),
+    install_requires=[  # External dependencies
+        'mariadb',
+        'requests',
+    ],
+    classifiers=[  # Optional classifiers
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
